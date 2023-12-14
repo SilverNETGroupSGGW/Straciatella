16461d13a25ae1fb60ab328d5b30536551d1f403
thephaseless <kukubaorch@gmail.com>
2023-12-14 01:05:46 +0100

    added tabs with dynamic days

--- lib/presentation/screens/navbar_screen.dart
+++ lib/presentation/screens/navbar_screen.dart
@@ -1,8 +1,12 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:psggw/data/mocks/schedules.dart';
+import 'package:psggw/data/models/lesson/lesson.dart';
+import 'package:psggw/data/models/schedule/schedule.dart';
 import 'package:psggw/logic/account/account_bloc.dart';
-import 'package:psggw/presentation/screens/timeline_screen.dart';
+import 'package:psggw/presentation/screens/map_screen.dart';
+import 'package:psggw/presentation/widgets/timeline/timeline.dart';
 
 class NavBarScreen extends StatefulWidget {
   NavBarScreen({super.key, required this.index});
@@ -19,9 +23,17 @@ class _NavBarScreenState extends State<NavBarScreen> {
   }
 
   late int selectedIndex;
+  Schedule schedule = schedules[0];
 
   @override
   Widget build(BuildContext context) {
+    List<DayOfWeek> days = [];
+    schedule.lessons.forEach((element) {
+      if (!days.contains(element.day)) {
+        days.add(element.day);
+      }
+    });
+
     bool isTimeline = selectedIndex == 0;
     return BlocListener<AccountBloc, AccountState>(
       listener: (context, state) {
@@ -50,32 +62,59 @@ class _NavBarScreenState extends State<NavBarScreen> {
           loggedIn: (_) => true,
         );
       },
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(isTimeline ? 'timeline'.tr() : 'map'.tr()),
-          actions: [
-            SettingsButton(),
-          ],
-        ),
-        body: isTimeline ? Timeline() : Placeholder(),
-        bottomNavigationBar: NavigationBar(
-          destinations: [
-            NavigationDestination(
-              icon:
-                  Icon(isTimeline ? Icons.dashboard : Icons.dashboard_outlined),
-              label: 'timeline'.tr(),
-            ),
-            NavigationDestination(
-              icon: Icon(isTimeline ? Icons.map_outlined : Icons.map),
-              label: 'map'.tr(),
-            ),
-          ],
-          selectedIndex: selectedIndex,
-          onDestinationSelected: (index) {
-            setState(() {
-              selectedIndex = index;
-            });
-          },
+      child: DefaultTabController(
+        length: days.length,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(isTimeline ? 'timeline'.tr() : 'map'.tr()),
+            actions: [
+              SettingsButton(),
+            ],
+            bottom: isTimeline
+                ? TabBar(
+                    tabs: days
+                        .map(
+                          (e) => Tab(
+                            text: e.name.tr(),
+                          ),
+                        )
+                        .toList(),
+                  )
+                : null,
+          ),
+          body: isTimeline
+              ? TabBarView(
+                  children: days.map(
+                    (day) {
+                      return Timeline(
+                        lessons: schedule.lessons
+                            .where((element) => element.day == day)
+                            .toList(),
+                      );
+                    },
+                  ).toList(),
+                )
+              : MapScreen(),
+          bottomNavigationBar: NavigationBar(
+            labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+            destinations: [
+              NavigationDestination(
+                icon: Icon(
+                    isTimeline ? Icons.dashboard : Icons.dashboard_outlined),
+                label: 'timeline'.tr(),
+              ),
+              NavigationDestination(
+                icon: Icon(isTimeline ? Icons.map_outlined : Icons.map),
+                label: 'map'.tr(),
+              ),
+            ],
+            selectedIndex: selectedIndex,
+            onDestinationSelected: (index) {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+          ),
         ),
       ),
     );
