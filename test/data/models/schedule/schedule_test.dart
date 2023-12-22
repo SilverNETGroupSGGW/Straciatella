fea8e3afa7a0b96acb8b8f07c5286a61a75f80a0
thephaseless <kukubaorch@gmail.com>
2023-12-22 22:58:10 +0100

    changed tested file xd

--- test/data/models/schedule/schedule_test.dart
+++ test/data/models/schedule/schedule_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:silvertimetable/data/models/schedule/schedule.dart';
 
 void main() {
-  testWidgets('schedule.freezed ...', (tester) async {
+  testWidgets('schedule ...', (tester) async {
     final json = {
       "id": "ba0d9a27-3078-4709-81e6-2d8b4e1c8a71",
       "created": "2023-11-27T00:00:06.6281617",
