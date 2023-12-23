3b8d260269150e45ca66968a80db3edd43784e16
Bartosz <barkwa67@gmail.com>
2023-12-23 23:45:14 +0100

    add ThemeType Hive adapter

--- lib/data/adapters/register_adapters.dart
+++ lib/data/adapters/register_adapters.dart
@@ -1,8 +1,10 @@
 import 'package:hive/hive.dart';
 import 'package:silvertimetable/data/adapters/color_adapter.dart';
 import 'package:silvertimetable/data/adapters/theme_mode_adapter.dart';
+import 'package:silvertimetable/data/adapters/theme_type_adapter.dart';
 
 registerDataAdapters() {
   Hive.registerAdapter(ColorAdapter());
   Hive.registerAdapter(ThemeModeAdapter());
+  Hive.registerAdapter(ThemeTypeAdapter());
 }
--- lib/data/hiveTypeIds.dart
+++ lib/data/hiveTypeIds.dart
@@ -1,6 +1,7 @@
 // *** values must be unique
 class HiveTypeIds {
   static const settingsState = 0;
+  static const themeType = 1;
   static const themeMode = 2;
   static const color = 3;
 }
