4cbd5ddf940793da02201f648e1c48d1809a3f97
Bartosz <barkwa67@gmail.com>
2023-12-29 22:21:06 +0100

    specify Future return type

--- lib/data/providers/sggw_hub_api.dart
+++ lib/data/providers/sggw_hub_api.dart
@@ -5,12 +5,12 @@ import 'package:silvertimetable/constants.dart';
 class SggwHubApi {
   final dio = Dio();
 
-  Future getLecturers() async => dio.getUri(Uri.parse('$apiUrl/Lecturers'));
+  Future<Response> getLecturers() async => dio.getUri(Uri.parse('$apiUrl/Lecturers'));
 
-  Future getLecturer(String id) async => dio.getUri(Uri.parse('$apiUrl/Lecturers/$id/schedule/full'));
+  Future<Response>getLecturer(String id) async => dio.getUri(Uri.parse('$apiUrl/Lecturers/$id/schedule/full'));
 
-  Future getSchedules() async => dio.getUri(Uri.parse('$apiUrl/Schedules'));
+  Future<Response>  getSchedules() async => dio.getUri(Uri.parse('$apiUrl/Schedules'));
 
-  Future getSchedule(String id) async => dio.getUri(Uri.parse('$apiUrl/Schedules/$id'));
+  Future<Response> getSchedule(String id) async => dio.getUri(Uri.parse('$apiUrl/Schedules/$id'));
 
 }
