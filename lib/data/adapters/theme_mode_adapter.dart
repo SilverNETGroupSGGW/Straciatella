72cd796f5e0fce434c195d01b2cc2794990597cd
matyjb <matyjb14@gmail.com>
2023-12-30 14:50:02 +0100

    fix

--- lib/data/adapters/theme_mode_adapter.dart
+++ lib/data/adapters/theme_mode_adapter.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:silvertimetable/data/adapters/enum_adapter.dart';
+import 'package:hive/hive.dart';
 import 'package:silvertimetable/data/hiveTypeIds.dart';
 
-class ThemeModeAdapter extends EnumAdapter<ThemeMode> {
+class ThemeModeAdapter extends TypeAdapter<ThemeMode> {
   @override
   final typeId = HiveTypeIds.themeMode;
+
+  @override
+  ThemeMode read(BinaryReader reader) {
+    return ThemeMode.values[reader.read()];
+  }
+
+  @override
+  void write(BinaryWriter writer, ThemeMode obj) {
+    writer.write(obj.index);
+  }
 }
--- lib/data/adapters/theme_type_adapter.dart
+++ lib/data/adapters/theme_type_adapter.dart
@@ -1,8 +1,18 @@
-import 'package:silvertimetable/data/adapters/enum_adapter.dart';
+import 'package:hive/hive.dart';
 import 'package:silvertimetable/data/hiveTypeIds.dart';
 import 'package:silvertimetable/themes.dart';
 
-class ThemeTypeAdapter extends EnumAdapter<ThemeType> {
+class ThemeTypeAdapter extends TypeAdapter<ThemeType> {
   @override
   final typeId = HiveTypeIds.themeType;
+
+  @override
+  ThemeType read(BinaryReader reader) {
+    return ThemeType.values[reader.read()];
+  }
+
+  @override
+  void write(BinaryWriter writer, ThemeType obj) {
+    writer.write(obj.index);
+  }
 }
