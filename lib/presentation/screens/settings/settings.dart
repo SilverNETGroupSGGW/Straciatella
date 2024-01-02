7a9e13812006403e1940068f3b5e27b90451f984
ThePhaseless <kukubaorch@gmail.com>
2024-01-02 13:33:48 +0100

    Add translations and update settings screen

--- lib/presentation/screens/settings/settings.dart
+++ lib/presentation/screens/settings/settings.dart
@@ -1,10 +1,5 @@
-// ignore_for_file: unused_import
-
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:silvertimetable/constants.dart';
-import 'package:silvertimetable/logic/settings/settings_cubit.dart';
 import 'package:silvertimetable/presentation/screens/settings/widgets/about_tile.dart';
 import 'package:silvertimetable/presentation/screens/settings/widgets/auto_theme_mode_tile.dart';
 import 'package:silvertimetable/presentation/screens/settings/widgets/color_tile.dart';
