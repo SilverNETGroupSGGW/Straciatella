48c2c69610570a93e11f17e7270f7ac967662ecc
thephaseless <kukubaorch@gmail.com>
2024-01-05 00:47:54 +0100

    Refactor welcome stage to handle all stages

--- lib/presentation/screens/welcome/welcome.dart
+++ lib/presentation/screens/welcome/welcome.dart
@@ -1,11 +1,8 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:silvertimetable/presentation/screens/welcome/widgets/customize_stage.dart';
-import 'package:silvertimetable/presentation/screens/welcome/widgets/final_stage.dart';
-import 'package:silvertimetable/presentation/screens/welcome/widgets/map_stage.dart';
-import 'package:silvertimetable/presentation/screens/welcome/widgets/notifications_stage.dart';
 import 'package:silvertimetable/presentation/screens/welcome/widgets/page_dots.dart';
-import 'package:silvertimetable/presentation/screens/welcome/widgets/schedules_stage.dart';
 import 'package:silvertimetable/presentation/screens/welcome/widgets/welcome_stage.dart';
+import 'package:silvertimetable/router.dart';
 
 // ignore_for_file: public_member_api_docs
 void main() {
@@ -30,24 +27,74 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
-  final controller = PageController(
-    viewportFraction: 0.8,
-    keepPage: true,
-  );
+  final controller = PageController();
 
   int currentPage = 0;
-
-  final pages = <Widget>[
-    WelcomeStage(),
-    SchedulesStage(),
-    NotificationsStage(),
-    MapStage(),
-    CustomizeStage(),
-    FinalStage(),
-  ];
+  static const iconSize = 150.0;
 
   @override
   Widget build(BuildContext context) {
+    final pages = <Widget>[
+      WelcomeStage(
+        header: Image(
+          image: AssetImage('assets/icon/icon.png'),
+          width: 200,
+          height: 200,
+        ),
+        title: 'welcome_stage_text'.tr(),
+        desc: 'welcome_stage_desc'.tr(),
+      ),
+      WelcomeStage(
+        header: Icon(
+          Icons.search,
+          size: iconSize,
+        ),
+        title: 'schedules_stage_text'.tr(),
+        desc: 'schedules_stage_desc'.tr(),
+      ),
+      WelcomeStage(
+        header: Icon(
+          // TODO: Change icon to off depending on the permission
+          Icons.notifications,
+          size: iconSize,
+        ),
+        title: 'notifications_stage_text'.tr(),
+        desc: 'notifications_stage_desc'.tr(),
+        leading: FilledButton(
+            onPressed: () {}, child: Text('allow_notifications'.tr())),
+      ),
+      WelcomeStage(
+        header: Icon(
+          Icons.map,
+          size: iconSize,
+        ),
+        title: 'map_stage_text'.tr(),
+        desc: 'map_stage_desc'.tr(),
+      ),
+      WelcomeStage(
+        header: Icon(
+          Icons.palette,
+          size: iconSize,
+        ),
+        title: 'customization_stage_text'.tr(),
+        desc: 'customization_stage_desc'.tr(),
+        leading: FilledButton(
+            onPressed: () {
+              // TODO: Navigate to theme screen instead of settings
+              Navigator.of(context).pushNamed(RouteNames.settings);
+            },
+            child: Text('customize'.tr())),
+      ),
+      WelcomeStage(
+        header: Icon(
+          Icons.favorite,
+          size: iconSize,
+        ),
+        title: 'final_stage_text'.tr(),
+        desc: 'final_stage_desc'.tr(),
+      ),
+    ];
+
     return PopScope(
       canPop: false,
       child: Scaffold(
@@ -59,7 +106,10 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   controller: controller,
                   itemCount: pages.length,
                   itemBuilder: (context, index) {
-                    return pages[index];
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                      child: pages[index],
+                    );
                   },
                   onPageChanged: (index) {
                     setState(() {
