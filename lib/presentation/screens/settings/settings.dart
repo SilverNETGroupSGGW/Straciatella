80769bf9115facf4fddc84875f369bd0ca51dcc0
ThePhaseless <kukubaorch@gmail.com>
2024-01-04 02:42:43 +0100

    Refactor settings screen layout

--- lib/presentation/screens/settings/settings.dart
+++ lib/presentation/screens/settings/settings.dart
@@ -1,9 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:silvertimetable/presentation/screens/settings/widgets/about_tile.dart';
-import 'package:silvertimetable/presentation/screens/settings/widgets/auto_theme_mode_tile.dart';
-import 'package:silvertimetable/presentation/screens/settings/widgets/color_tile.dart';
-import 'package:silvertimetable/presentation/screens/settings/widgets/dark_theme_mode_tile.dart';
+import 'package:silvertimetable/presentation/screens/settings/widgets/customization_tile.dart';
 
 import '../../widgets/category_label.dart';
 import 'widgets/debug_widgets_tile.dart';
@@ -19,26 +17,30 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('settings'.tr()),
-      ),
-      body: ListView(
-        children: [
-          CategoryLabel(text: 'theme'.tr()),
-          AutoThemeModeTile(),
-          DarkThemeModeTile(),
-          ColorTile(),
-          CategoryLabel(text: 'schedules'.tr()),
-          ManageSchedulesTile(),
-          CategoryLabel(text: 'preferences'.tr()),
-          NotificationsTile(),
-          LanguageTile(),
-          ShowFabSwitchTile(),
-          CategoryLabel(text: 'debug'.tr()),
-          DebugWidgetsTile(),
-          CategoryLabel(text: 'about'.tr()),
-          PrivacyPolicyTile(),
-          AboutTile(),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar.large(
+            title: Text('settings'.tr()),
+            expandedHeight: 196,
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                CategoryLabel(text: 'schedules'.tr()),
+                const ManageSchedulesTile(),
+                CategoryLabel(text: 'preferences'.tr()),
+                const CustomizationTile(),
+                const NotificationsTile(),
+                const LanguageTile(),
+                const ShowFabSwitchTile(),
+                CategoryLabel(text: 'debug'.tr()),
+                const DebugWidgetsTile(),
+                CategoryLabel(text: 'about'.tr()),
+                const PrivacyPolicyTile(),
+                const AboutTile(),
+              ],
+            ),
+          ),
         ],
       ),
     );
