16657252de64be41da98633dccb7e462f2a06421
Bartosz <barkwa67@gmail.com>
2024-01-19 22:03:00 +0100

    remove unused import

--- lib/presentation/builders/schedule_manager_builders/schedule_manager_schedule_options_tree_builder.dart
+++ lib/presentation/builders/schedule_manager_builders/schedule_manager_schedule_options_tree_builder.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:silvertimetable/data/models/options_tree/options_tree_node.dart';
-import 'package:silvertimetable/data/types.dart';
 import 'package:silvertimetable/logic/schedule_manager/schedule_manager_bloc.dart';
 
 /// Use this builder to render schedule tree
