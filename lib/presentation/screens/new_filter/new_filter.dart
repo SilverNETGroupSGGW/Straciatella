2b7ceb5ae4b436ccffdf50de1f2ece70137d7fd6
Bartosz <barkwa67@gmail.com>
2024-01-24 13:17:52 +0100

    load optionsTree from scheduleManagerBloc

--- lib/presentation/screens/new_filter/new_filter.dart
+++ lib/presentation/screens/new_filter/new_filter.dart
@@ -1,5 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:silvertimetable/logic/schedule_manager/schedule_manager_bloc.dart';
 import 'package:silvertimetable/presentation/builders/schedule_manager_builders/schedule_manager_schedule_options_tree_builder.dart';
 
 class NewFilterScreen extends StatelessWidget {
@@ -7,14 +9,19 @@ class NewFilterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _scheduleManager = BlocProvider.of<ScheduleManagerBloc>(context)
+      ..add(const ScheduleManagerEvent.updateIndex());
+
     return Scaffold(
       appBar: AppBar(title: Text('new_filter_title'.tr())),
       body: ScheduleManagerScheduleOptionsTreeBuilder(
-        builder: (context, scheduleOptionsTree) => ListView(
-          children: [
-            Text('$scheduleOptionsTree'),
-          ],
-        ),
+        builder: (ctx, optionsTree) => optionsTree == null
+            ? const CircularProgressIndicator() // TODO coś bardziej fancy
+            : ListView(
+                children: [
+                  Text('${optionsTree}'),
+                ],
+              ),
       ),
     );
   }
