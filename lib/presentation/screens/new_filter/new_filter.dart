13671f052084d6c7078e0d8d6ef20065d4c99d87
Bartosz <barkwa67@gmail.com>
2024-01-25 22:04:38 +0100

    create widget for presenting adequate icon, present options row

--- lib/presentation/screens/new_filter/new_filter.dart
+++ lib/presentation/screens/new_filter/new_filter.dart
@@ -1,8 +1,11 @@
+import 'dart:collection';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:silvertimetable/logic/schedule_manager/schedule_manager_bloc.dart';
 import 'package:silvertimetable/presentation/builders/schedule_manager_builders/schedule_manager_schedule_options_tree_builder.dart';
+import 'package:silvertimetable/presentation/screens/new_filter/widgets/options_row.dart';
 
 class NewFilterScreen extends StatefulWidget {
   // ignore: prefer_const_constructors_in_immutables
@@ -29,10 +32,36 @@ class _NewFilterScreenState extends State<NewFilterScreen> {
             ? const CircularProgressIndicator() // TODO coś bardziej fancy
             : ListView(
                 children: [
-                  Text('${optionsTree}'),
+                  //Text('$optionsTree'),
+                  NewFilterOptionsRow(
+                    icon: nodeIcon(optionsTree.name),
+                    setTitle: optionsTree.name.tr(),
+                    chipTitles: keyListFromOptions(optionsTree.options),
+                  ),
+
                 ],
               ),
       ),
     );
   }
+
+  IconData nodeIcon(String nodeName) {
+    switch (nodeName) {
+      case 'faculty':
+        return Icons.apartment;
+      case 'fieldOfStudy':
+        return Icons.build;
+      case 'studyMode':
+        return Icons.calendar_month;
+      case 'degreeOfStudy':
+        return Icons.school;
+      case 'semester':
+        return Icons.numbers;
+      default:
+        return Icons.school;
+    }
+  }
+
+  List<String> keyListFromOptions(SplayTreeMap options) =>
+      options.keys.map((key) => key as String).toList();
 }
