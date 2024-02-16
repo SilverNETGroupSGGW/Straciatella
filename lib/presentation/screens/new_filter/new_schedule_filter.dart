751770604f234b5fd477d8532b7aabcf1d0cf86b
Bartosz <barkwa67@gmail.com>
2024-02-16 20:25:42 +0100

    move filters list to separate widget

--- lib/presentation/screens/new_filter/new_schedule_filter.dart
+++ lib/presentation/screens/new_filter/new_schedule_filter.dart
@@ -3,9 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:silvertimetable/data/models/options_tree/options_tree_node.dart';
 import 'package:silvertimetable/logic/schedule_manager/schedule_manager_bloc.dart';
-import 'package:silvertimetable/presentation/screens/new_filter/widgets/add_new_filter_button.dart';
+import 'package:silvertimetable/presentation/screens/new_filter/widgets/filters_list.dart';
 import 'package:silvertimetable/presentation/screens/new_filter/widgets/filters_loading.dart';
-import 'package:silvertimetable/presentation/screens/new_filter/widgets/options_row.dart';
 
 class Choice {
   Choice({required this.level, required this.selected});
@@ -26,90 +25,33 @@ class NewScheduleFilterScreen extends StatefulWidget {
 class _NewScheduleFilterScreenState extends State<NewScheduleFilterScreen> {
   late List<Choice> userChoices;
 
-  final GlobalKey<AnimatedListState> _animatedListStateKey = GlobalKey();
-  final Duration _animateDuration = const Duration(milliseconds: 100);
-
   @override
   void initState() {
     super.initState();
     context
         .read<ScheduleManagerBloc>()
         .add(const ScheduleManagerEvent.updateIndex());
-
-    userChoices = [
-      Choice(
-        level: context.read<ScheduleManagerBloc>().state.schedulesOptionsTree,
-        selected: null,
-      ),
-    ];
-  }
-
-  void chipPressedCallback(OptionsTreeNode level, dynamic selectedKey) {
-    final choiceIndex =
-        userChoices.indexWhere((choice) => choice.level == level);
-
-    for (int i = userChoices.length - 1; i > choiceIndex; i--) {
-      animateRemovingItem(i);
-      userChoices.removeAt(i);
-    }
-
-    userChoices[choiceIndex].selected = selectedKey;
-    userChoices.add(Choice(level: level.options[selectedKey], selected: null));
-    animateAddingItem(userChoices.length - 1);
-    setState(() {});
-  }
-
-  void animateAddingItem(int index) {
-    _animatedListStateKey.currentState!
-        .insertItem(index, duration: _animateDuration);
-  }
-
-  void animateRemovingItem(int index) {
-    _animatedListStateKey.currentState!.removeItem(
-      index,
-      (_, animation) => SizeTransition(
-        sizeFactor: animation,
-      ),
-      duration: _animateDuration,
-    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('new_filter_title'.tr())),
-      body: BlocConsumer<ScheduleManagerBloc, ScheduleManagerState>(
-        listener: (context, state) {
-          userChoices = [
-            Choice(level: state.schedulesOptionsTree, selected: null),
-          ];
-        },
+      body: BlocBuilder<ScheduleManagerBloc, ScheduleManagerState>(
         builder: (context, state) {
-          if (state.refreshingIndex || state.schedulesOptionsTree == null) {
-            // TODO: Informacja o ładowaniu (najnowszych) planów
+          if (state.refreshingIndex && state.schedulesOptionsTree != null) {
+            return Column(
+              children: [
+                const LinearProgressIndicator(),
+                const Spacer(),
+                FiltersList(optionsTree: state.schedulesOptionsTree!),
+              ],
+            );
+          }
+          if (state.schedulesOptionsTree == null) {
             return FiltersLoading();
           }
-          return Container(
-            alignment: Alignment.bottomCenter,
-            padding: const EdgeInsets.only(bottom: 20.0),
-            child: AnimatedList(
-              key: _animatedListStateKey,
-              initialItemCount: userChoices.length,
-              shrinkWrap: true,
-              itemBuilder: (context, index, animation) => SizeTransition(
-                sizeFactor: animation,
-                child: !userChoices[index].level!.isLeaf
-                    ? NewFilterOptionsRow(
-                        level: userChoices[index].level!,
-                        selectedKey: userChoices[index].selected,
-                        callback: chipPressedCallback,
-                      )
-                    : AddNewFilterButton(
-                        pickedId: userChoices.last.level!.leafValue.toString(),
-                      ),
-              ),
-            ),
-          );
+          return FiltersList(optionsTree: state.schedulesOptionsTree!);
         },
       ),
     );
---
+++ lib/presentation/screens/new_filter/widgets/filters_list.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:silvertimetable/data/models/options_tree/options_tree_node.dart';
+import 'package:silvertimetable/logic/schedule_manager/schedule_manager_bloc.dart';
+import 'package:silvertimetable/presentation/screens/new_filter/new_schedule_filter.dart';
+import 'package:silvertimetable/presentation/screens/new_filter/widgets/add_new_filter_button.dart';
+import 'package:silvertimetable/presentation/screens/new_filter/widgets/options_row.dart';
+
+// ignore: must_be_immutable
+class FiltersList extends StatefulWidget {
+  FiltersList({super.key, required this.optionsTree});
+
+  OptionsTreeNode optionsTree;
+
+  @override
+  State<FiltersList> createState() => _FiltersListState();
+}
+
+class _FiltersListState extends State<FiltersList> {
+  late List<Choice> userChoices;
+
+  final GlobalKey<AnimatedListState> _animatedListStateKey = GlobalKey();
+  final Duration _animateDuration = const Duration(milliseconds: 100);
+
+  void chipPressedCallback(OptionsTreeNode level, dynamic selectedKey) {
+    final choiceIndex =
+        userChoices.indexWhere((choice) => choice.level == level);
+
+    for (int i = userChoices.length - 1; i > choiceIndex; i--) {
+      animateRemovingItem(i);
+      userChoices.removeAt(i);
+    }
+
+    userChoices[choiceIndex].selected = selectedKey;
+    userChoices.add(Choice(level: level.options[selectedKey], selected: null));
+    animateAddingItem(userChoices.length - 1);
+    setState(() {});
+  }
+
+  void animateAddingItem(int index) {
+    _animatedListStateKey.currentState!
+        .insertItem(index, duration: _animateDuration);
+  }
+
+  void animateRemovingItem(int index) {
+    _animatedListStateKey.currentState!.removeItem(
+      index,
+      (_, animation) => SizeTransition(
+        sizeFactor: animation,
+      ),
+      duration: _animateDuration,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    userChoices = [
+      Choice(
+        level: widget.optionsTree,
+        selected: null,
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.bottomCenter,
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: BlocListener<ScheduleManagerBloc, ScheduleManagerState>(
+        listener: (context, state) {
+          userChoices = [
+            Choice(level: state.schedulesOptionsTree, selected: null),
+          ];
+        },
+        child: AnimatedList(
+          key: _animatedListStateKey,
+          initialItemCount: userChoices.length,
+          shrinkWrap: true,
+          itemBuilder: (context, index, animation) => SizeTransition(
+            sizeFactor: animation,
+            child: !userChoices[index].level!.isLeaf
+                ? NewFilterOptionsRow(
+                    level: userChoices[index].level!,
+                    selectedKey: userChoices[index].selected,
+                    callback: chipPressedCallback,
+                  )
+                : AddNewFilterButton(
+                    pickedId: userChoices.last.level!.leafValue.toString(),
+                  ),
+          ),
+        ),
+      ),
+    );
+  }
+}
