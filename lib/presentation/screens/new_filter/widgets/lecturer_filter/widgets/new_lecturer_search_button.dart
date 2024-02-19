a9e6f11527146edab3b556bdce159ca35510cb58
mpygruu <barkwa67@gmail.com>
2024-02-19 22:32:52 +0100

    initial (not working yet) lecturer searching flow

--- lib/presentation/screens/new_filter/widgets/lecturer_filter/widgets/new_lecturer_search_button.dart
+++ lib/presentation/screens/new_filter/widgets/lecturer_filter/widgets/new_lecturer_search_button.dart
@@ -1,12 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:silvertimetable/data/models/lecturer/lecturer_base.dart';
+import 'package:silvertimetable/logic/schedule_manager/schedule_manager_bloc.dart';
 
-class NewLecturerSearchButton extends StatelessWidget {
+class NewLecturerSearchButton extends StatefulWidget {
   const NewLecturerSearchButton({super.key});
 
   @override
+  State<NewLecturerSearchButton> createState() =>
+      _NewLecturerSearchButtonState();
+}
+
+class _NewLecturerSearchButtonState extends State<NewLecturerSearchButton> {
+  @override
   Widget build(BuildContext context) {
-    return IconButton(onPressed: () {}, icon: const Icon(Icons.search));
+    // TODO: clean the code and fix showing suggestions and results when searching
+
+    final List<LecturerBase> lecturers = context
+        .read<ScheduleManagerBloc>()
+        .state
+        .schedulesIndex
+        .values
+        .whereType<LecturerBase>()
+        .toList();
+
+    return IconButton(
+      onPressed: () {
+        showSearch(
+          context: context,
+          delegate: NewLecturerSearchDelegate(lecturers),
+        );
+      },
+      icon: const Icon(Icons.search),
+    );
+  }
+}
+
+class NewLecturerSearchDelegate extends SearchDelegate {
+  NewLecturerSearchDelegate(this.lecturers);
+
+  List<LecturerBase> lecturers;
+
+  String getPresentedInfo(LecturerBase lecturer) =>
+      '${lecturer.academicDegree} ${lecturer.firstName} ${lecturer.surname}';
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: const Icon(Icons.clear),
+      ),
+    ];
   }
 
-  // TODO: Implement searching and picking lecturer id
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: const Icon(Icons.arrow_back),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    final List<LecturerBase> matched = [];
+    for (final lecturer in lecturers) {
+      if (getPresentedInfo(lecturer)
+          .toLowerCase()
+          .contains(query.toLowerCase())) {
+        matched.add(lecturer);
+      }
+    }
+    return ListView.builder(
+      itemCount: matched.length,
+      itemBuilder: (context, index) => ListTile(
+        title: Text(lecturers[index].toString()),
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final List<LecturerBase> matched = [];
+    for (final lecturer in lecturers) {
+      if (getPresentedInfo(lecturer)
+          .toLowerCase()
+          .contains(query.toLowerCase())) {
+        matched.add(lecturer);
+      }
+    }
+    return ListView.builder(
+      itemCount: matched.length,
+      itemBuilder: (context, index) => ListTile(
+        title: Text(getPresentedInfo(lecturers[index])),
+      ),
+    );
+  }
 }
