f92af0c51c49ba51abe5d7db5907b462b0aa4ba7
matyjb <matyjb14@gmail.com>
2024-03-03 13:42:36 +0100

    regenerate files

--- lib/data/models/classroom/classroom.freezed.dart
+++ lib/data/models/classroom/classroom.freezed.dart
@@ -12,7 +12,7 @@ part of 'classroom.dart';
 T _$identity<T>(T value) => value;
 
 final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
 Classroom _$ClassroomFromJson(Map<String, dynamic> json) {
   return _Classroom.fromJson(json);
@@ -211,7 +211,7 @@ class _$ClassroomImpl extends _Classroom {
   }
 
   @override
-  bool operator ==(dynamic other) {
+  bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is _$ClassroomImpl &&
