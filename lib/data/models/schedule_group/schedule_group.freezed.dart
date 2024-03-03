c2e91caa24ea2330ed95b82f77631e5838436f66
matyjb <matyjb14@gmail.com>
2024-03-03 13:42:36 +0100

    regenerate files

--- lib/data/models/schedule_group/schedule_group.freezed.dart
+++ lib/data/models/schedule_group/schedule_group.freezed.dart
@@ -12,7 +12,7 @@ part of 'schedule_group.dart';
 T _$identity<T>(T value) => value;
 
 final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
 ScheduleGroup _$ScheduleGroupFromJson(Map<String, dynamic> json) {
   return _ScheduleGroup.fromJson(json);
@@ -195,7 +195,7 @@ class _$ScheduleGroupImpl extends _ScheduleGroup {
   }
 
   @override
-  bool operator ==(dynamic other) {
+  bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is _$ScheduleGroupImpl &&
