e10b33ef74eb569960cee1bb6714c2d524903d64
matyjb <matyjb14@gmail.com>
2024-03-04 22:23:00 +0100

    wip

--- lib/presentation/screens/new_schedule/widgets/student_schedule/widgets/student_schedules_stack.dart
+++ lib/presentation/screens/new_schedule/widgets/student_schedule/widgets/student_schedules_stack.dart
@@ -16,73 +16,54 @@ class StudentSchedulesStack extends StatefulWidget {
 class _StudentSchedulesStackState extends State<StudentSchedulesStack> {
   final GlobalKey<AnimatedListState> animatedListKey = GlobalKey();
   final Duration animatedDuration = const Duration(milliseconds: 200);
-  int animatedListRowsCount = 1;
-
-final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
-  late ListModel<int> _list;
-  int? _selectedItem;
-  late int
-      _nextItem; // The next item inserted when the user presses the '+' button.
+  late ListModel<OptionsTreeNode> _levels;
 
   @override
   void initState() {
     super.initState();
-    _list = ListModel<int>(
-      listKey: _listKey,
-      initialItems: <int>[0, 1, 2],
-      removedItemBuilder: _buildRemovedItem,
+    _levels = ListModel<OptionsTreeNode>(
+      listKey: animatedListKey,
+      initialItems: <OptionsTreeNode>[],
+      removedItemBuilder: (level, context, animation) =>
+          _buildLevel(context, _levels.indexOf(level), animation),
     );
-    _nextItem = 3;
   }
 
   // Used to build list items that haven't been removed.
-  Widget _buildItem(
-      BuildContext context, int index, Animation<double> animation,) {
-    return CardItem(
-      animation: animation,
-      item: _list[index],
-      selected: _selectedItem == _list[index],
-      onTap: () {
-        setState(() {
-          _selectedItem = _selectedItem == _list[index] ? null : _list[index];
-        });
-      },
-    );
-  }
-
-  /// The builder function used to build items that have been removed.
-  ///
-  /// Used to build an item after it has been removed from the list. This method
-  /// is needed because a removed item remains visible until its animation has
-  /// completed (even though it's gone as far as this ListModel is concerned).
-  /// The widget will be used by the [AnimatedListState.removeItem] method's
-  /// [AnimatedRemovedItemBuilder] parameter.
-  Widget _buildRemovedItem(
-      int item, BuildContext context, Animation<double> animation,) {
-    return CardItem(
-      animation: animation,
-      item: item,
-      // No gesture detector here: we don't want removed items to be interactive.
+  Widget _buildLevel(
+    BuildContext context,
+    int index,
+    Animation<double> animation,
+  ) {
+    return SizeTransition(
+      sizeFactor: animation,
+      child: (_levels[index].isLeaf)
+          ? null
+          : StudentScheduleOptionsRow(
+              choiceIndex: index,
+              level: _levels[index],
+            ),
     );
   }
 
   // Insert the "next item" into the list model.
-  void _insert() {
-    final int index =
-        _selectedItem == null ? _list.length : _list.indexOf(_selectedItem!);
-    _list.insert(index, _nextItem++);
+  void _insertLast(OptionsTreeNode level) {
+    // todo:
+    // final int index =
+    //     _selectedItem == null ? _list.length : _list.indexOf(_selectedItem!);
+    // _list.insert(index, _nextItem++);
   }
 
   // Remove the selected item from the list model.
-  void _remove() {
-    if (_selectedItem != null) {
-      _list.removeAt(_list.indexOf(_selectedItem!));
-      setState(() {
-        _selectedItem = null;
-      });
-    }
+  void _removeLast() {
+    // todo:
+    // if (_selectedItem != null) {
+    //   _list.removeAt(_list.indexOf(_selectedItem!));
+    //   setState(() {
+    //     _selectedItem = null;
+    //   });
+    // }
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -115,35 +96,11 @@ final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
         builder: (context, userChoicesState) {
           return BlocBuilder<ScheduleManagerBloc, ScheduleManagerState>(
             builder: (context, state) {
-              final ListModel<int> listModel = ListModel(listKey: animatedListKey, removedItemBuilder: removedItemBuilder)
-
               return AnimatedList(
                 key: animatedListKey,
                 shrinkWrap: true,
-                initialItemCount: animatedListRowsCount,
-                itemBuilder: (context, index, animation) =>
-                    BlocBuilder<ScheduleManagerBloc, ScheduleManagerState>(
-                  builder: (context, state) {
-                    int t = 0;
-                    OptionsTreeNode? level = state.schedulesOptionsTree;
-                    if (userChoicesState.pickedKeys.isNotEmpty) {
-                      while (t <= index && level != null) {
-                        level =
-                            level.options[userChoicesState.pickedKeys[index]];
-                        t++;
-                      }
-                    }
-                    return SizeTransition(
-                      sizeFactor: animation,
-                      child: (level == null || level.isLeaf)
-                          ? null
-                          : StudentScheduleOptionsRow(
-                              choiceIndex: index,
-                              level: level,
-                            ),
-                    );
-                  },
-                ),
+                initialItemCount: _levels.length,
+                itemBuilder: _buildLevel,
               );
             },
           );
