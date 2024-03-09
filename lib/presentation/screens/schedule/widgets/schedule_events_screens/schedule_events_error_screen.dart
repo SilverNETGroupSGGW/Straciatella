182b81fa8dadf7b28e263d78acef9c21937ba99e
mpygruu <barkwa67@gmail.com>
2024-03-09 10:07:37 +0100

    subtitle in appbar

--- lib/presentation/screens/schedule/widgets/schedule_events_screens/schedule_events_error_screen.dart
+++ lib/presentation/screens/schedule/widgets/schedule_events_screens/schedule_events_error_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:material_symbols_icons/symbols.dart';
@@ -23,14 +26,17 @@ class ScheduleEventsErrorScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Column(
+          crossAxisAlignment: (!kIsWeb && Platform.isIOS)
+              ? CrossAxisAlignment.center
+              : CrossAxisAlignment.start,
           children: [
             Text(
               'schedule'.tr(),
               style: const TextStyle(fontSize: 24),
             ),
             Text(
-              scheduleEventsCubit.,
-              style: const TextStyle(fontSize: 15),
+              scheduleEventsCubit.state.fromSchedule!.toPrettyString(),
+              style: const TextStyle(fontSize: 12),
             ),
           ],
         ),
