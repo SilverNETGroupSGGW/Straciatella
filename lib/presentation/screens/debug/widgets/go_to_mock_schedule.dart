c79ae906d06662adead6a39267f49b082cd889a2
ThePhaseless <kukubaorch@gmail.com>
2024-03-10 14:38:17 +0000

    changed buttons to listtiles

--- lib/presentation/screens/debug/widgets/go_to_mock_schedule.dart
+++ lib/presentation/screens/debug/widgets/go_to_mock_schedule.dart
@@ -3,22 +3,22 @@ import 'package:silvertimetable/data/fakes/mock_jsons.dart';
 import 'package:silvertimetable/presentation/screens/schedule/schedule_screen.dart';
 import 'package:silvertimetable/router.dart';
 
-class GoToMockSchedule extends StatelessWidget {
-  const GoToMockSchedule({
+class ShowMockScheduleScreen extends StatelessWidget {
+  const ShowMockScheduleScreen({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () => Navigator.pushNamed(
+    return ListTile(
+      onTap: () => Navigator.pushNamed(
         context,
         RouteNames.schedule,
         arguments: const ScheduleScreenArgs(
           mockScheduleKey,
         ),
       ),
-      child: const Text("go to test schedule"),
+      title: const Text("go to test schedule"),
     );
   }
 }
