201d6cfea86dd0731927283fa4f274014fe51f73
mpygruu <barkwa67@gmail.com>
2024-09-29 17:55:27 +0200

    Add translation dependent strings

--- lib/presentation/screens/schedule/widgets/schedule_filter_fab/schedule_filter_option.dart
+++ lib/presentation/screens/schedule/widgets/schedule_filter_fab/schedule_filter_option.dart
@@ -1,6 +1,8 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:material_symbols_icons/symbols.dart';
 import 'package:silvertimetable/data/models/enums.dart';
+import 'package:silvertimetable/presentation/screens/schedule/widgets/schedule_events_screens/schedule_events_empty_screen.dart';
 
 class ScheduleFilterOption extends StatelessWidget {
   final ScheduleFilter filterType;
@@ -23,9 +25,9 @@ class ScheduleFilterOption extends StatelessWidget {
   }
 
   String get filterOptionText => switch (filterType) {
-        ScheduleFilter.studyProgram => 'change_schedule',
-        ScheduleFilter.semester => 'change_semester',
-        ScheduleFilter.group => 'change_group',
+        ScheduleFilter.studyProgram => 'change_schedule'.tr(),
+        ScheduleFilter.semester => 'change_semester'.tr(),
+        ScheduleFilter.group => 'change_group'.tr(),
       };
 
   IconData get filterIcon => switch (filterType) {
