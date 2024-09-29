0eda1fd73e7c10b8c5d0b78a01c496516fb48b99
mpygruu <barkwa67@gmail.com>
2024-09-29 18:55:43 +0200

    Remove unused import

--- lib/presentation/screens/schedule/widgets/schedule_filter_fab/schedule_filter_option.dart
+++ lib/presentation/screens/schedule/widgets/schedule_filter_fab/schedule_filter_option.dart
@@ -2,7 +2,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:material_symbols_icons/symbols.dart';
 import 'package:silvertimetable/data/models/enums.dart';
-import 'package:silvertimetable/presentation/screens/schedule/widgets/schedule_events_screens/schedule_events_empty_screen.dart';
 
 class ScheduleFilterOption extends StatelessWidget {
   final ScheduleFilter filterType;
